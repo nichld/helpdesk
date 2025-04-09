fa6b7ccb89ad9765ba9eb32ceb2cef9702a8812a
Nichlas <141312928+nichld@users.noreply.github.com>
2025-04-09 10:47:21 +0200

    Bind server to '0.0.0.0' in startServer function for external access

--- src/utils/server.js
+++ src/utils/server.js
@@ -5,7 +5,7 @@
 exports.startServer = (server) => {
   const port = process.env.PORT || process.env.ALTERNATIVE_PORT || 3000;
   
-  server.listen(port, () => {
+  server.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
     console.log(`Access the application at http://localhost:${port}`);
   });
